2edb00b3daf9cf5927817fdd1a568ba80ea25c78
0xStacker <eliot.bourdariat@gmail.com>
2025-10-25 00:09:04 +0800

    gradients computed (by hand) , weight struct added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 #include <iostream>
 #include <cstdlib>
 
-int main() {
-    Eigen::Matrix2d A; A << 1,2,3,4;
-    Eigen::Vector2d b(5,6);
-    std::cout << "A*b =\n" << A*b << "\n";
+static int H = 32;
+static int D = 256;
+
+struct Weights
+{
+    // Eigen::MatrixXd W1(256,H); its not gonna call the constructor
+    Eigen::MatrixXd W1;
+    Eigen::MatrixXd b1;
+    Eigen::MatrixXd W2;
+    Eigen::MatrixXd b2;
+    Weights() : W1(D, H), b1(1,H),W2(H,D),b2(1,256)  {};
+};
+
+int main()
+{
     
     return 0;
 }
